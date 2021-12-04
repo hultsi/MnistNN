ed87597118fedba7f16c4b89802eba7a7da2aa2a
Miro R <miroruopsa@gmail.com>
2021-12-04 17:01:16 +0200

    touch screen implementation for front end

--- front-end/paint.js
+++ front-end/paint.js
@@ -17,11 +17,19 @@ const clearCanvas = function() {
 }
 
 const getMousePos = function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
+    const rect = canvas.getBoundingClientRect();
+    if (evt.constructor.name === "MouseEvent") {
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    } else if (evt.constructor.name === "TouchEvent") {
+        console.log(evt);
+        return {
+            x: evt.touches[0].clientX - rect.left,
+            y: evt.touches[0].clientY - rect.top
+        };
+    }
 }
 
 const onMouseDown = function onMouseDown(evt, canvas) {
@@ -36,19 +44,25 @@ const onMouseDown = function onMouseDown(evt, canvas) {
 
 const onMouseUp = function onMouseUp() {
     isMouseDown = false;
+    if (window.getSelection) {window.getSelection().removeAllRanges();}
+    else if (document.selection) {document.selection.empty();}
 }
 
 const onMouseMove = function onMouseMove(evt, canvas) {
+    evt.preventDefault();
     if (isMouseDown) {
-        var currentPosition = getMousePos(canvas, evt);
+        const currentPosition = getMousePos(canvas, evt);
         ctx.lineTo(currentPosition.x, currentPosition.y)
         ctx.stroke();
     }
 }
 
-document.getElementById('clearCache').addEventListener('click', clearCanvas);
-canvas.addEventListener('mousedown', (e) => onMouseDown(e, canvas));
-canvas.addEventListener('mousemove', (e) => onMouseMove(e, canvas));
-canvas.addEventListener('mouseup', onMouseUp);
+document.getElementById("clearCache").addEventListener("click", clearCanvas);
+canvas.addEventListener("mousedown", (e) => onMouseDown(e, canvas));
+canvas.addEventListener("mousemove", (e) => onMouseMove(e, canvas));
+canvas.addEventListener("mouseup", onMouseUp);
+canvas.addEventListener("touchstart", (e) => onMouseDown(e, canvas));
+canvas.addEventListener("touchmove", (e) => onMouseMove(e, canvas));
+canvas.addEventListener("touchend", (e) => onMouseUp(e, canvas));
 
 clearCanvas();
